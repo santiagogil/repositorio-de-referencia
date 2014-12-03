862b41c49faf3a5acf0caf13fe4e868b20c206cc
Santiago Gil <gil.educaciontdf@gmail.com>
2014-12-03 19:06:21 +0000

    Se agrega el primer modulo de ejemplo.
    En el archivo index.js se incluye un modulo suma con la correspondiente documentacion para ser parseada por jsdocs y un error intencional para demostrar el funcionamiento de jshint.

---
+++ index.js
@@ -0,0 +1,14 @@
+/** @module suma */
+
+/**
+ * Suma dos nx&uacute;meros.
+ * @function
+ * @name suma
+ * @param {number} a - Primer n&uacute;mero a sumar.
+ * @param {number} b - Segundo n&uacute;mero a sumar.
+ * @returns {number} - La suma de a y b
+ */
+
+module.exports = function suma(a, b) {
+  return a + b;
+}
